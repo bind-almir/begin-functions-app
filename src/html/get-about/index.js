1d441e6d3183b319b8e44e929e52e56402725e07
A.Z <bind-almir@users.noreply.github.com>
2018-09-25 20:36:26 +0200

    Update index.js

--- src/html/get-about/index.js
+++ src/html/get-about/index.js
@@ -6,7 +6,7 @@ let defaultHTML = `
 <html lang=en>
   <head>
     <meta charset=utf-8>
-    <title>Hi!</title>
+    <title>Wow!</title>
   </head>
   <body style="font-family: sans-serif;">
     <h1>Hello world!</h1>
